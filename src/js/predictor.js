fbfe364f5fd7f9419d27d1595257d811bd83d0c9
njd80 <neil.donaldson04@gmail.com>
2018-06-06 01:28:22 +0100

    Add basic Group & Teams Display

--- src/js/predictor.js
+++ src/js/predictor.js
@@ -1,16 +1,50 @@
 predictor = {
-  init: function() {
-    console.log('init');
 
-    //Load Data
+  dom: {
+    mainContainer: '#mainContainer',
+    groups: '#mainContainer .group',
+    log: '#log'
+  },
+
+  events: {
+
+  },
+
+  init: function() {
+    var parent = this;
     $.getJSON("data.json", function(d) {
-      predictor.processData(d);
+
+      //temp display json
+      $(parent.dom.log).html(JSON.stringify(d));
+
+      //Iterate Groups
+      $.each(d, function(i, group) {
+        parent.renderGroupContainer(i, group);
+        parent.renderTeams(i, group.teams);
+      });
     });
   },
 
-  processData: function(d) {
-    console.log(d);
-    
+  renderGroupContainer: function(index,group) {
+    var template = [
+      '<div class="group" id="'+index+'">',
+      ' <p class="name">'+group.name+'</p>',
+      ' <div class="teams"></div',
+      '</div>'
+    ].join("\n");
+    $(this.dom.mainContainer).append(template);
+  },
+
+  renderTeams: function(group,teams) {
+    var teamsContainer = $(this.dom.groups).filter('#'+group).find('.teams');
+    $.each(teams, function(i, team) {
+      var template = '<img src="img/'+team.short_name+'.png"/><p class="team" id="'+i+'">'+team.name+'</p>';
+      teamsContainer.append(template);
+    });
+  },
+
+  renderFixtures: function(group) {
+    console.log(group);
   }
 
 };
