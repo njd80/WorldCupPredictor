beed6e95271c2221dd4a254cfe9ba3aaeaa883a8
njd80 <neil.donaldson04@gmail.com>
2018-06-07 00:25:32 +0100

    Add display fixtures and remove log

--- src/js/predictor.js
+++ src/js/predictor.js
@@ -7,29 +7,38 @@ predictor = {
   },
 
   events: {
-
+    clickEvents: function() {
+      $('#mainContainer').on('click', '.fixturesLabel', function() {
+        console.log('fixturesLabel');
+			});
+    }
   },
 
   init: function() {
     var parent = this;
     $.getJSON("data/groups.json", function(d) {
 
-      //temp display json
-      $(parent.dom.log).html(JSON.stringify(d));
-
       //Iterate Groups
       $.each(d, function(i, group) {
         parent.renderGroupContainer(i, group);
         parent.renderTeams(i, group.teams);
+        parent.renderFixtures(i, group.teams);
       });
     });
+
+    //Add click events
+		parent.events.clickEvents();
   },
 
   renderGroupContainer: function(index,group) {
     var template = [
       '<div class="group" id="'+index+'">',
       ' <p class="name">'+group.name+'</p>',
-      ' <div class="teams"></div',
+      ' <div class="teams"></div>',
+      ' <p class="fixturesLabel">Fixtures',
+      '   <span class="fixturesExpandLabel">+</span>',
+      ' </p>',
+      ' <div class="fixtures"></div>',
       '</div>'
     ].join("\n");
     $(this.dom.mainContainer).append(template);
@@ -48,8 +57,23 @@ predictor = {
     });
   },
 
-  renderFixtures: function(group) {
-    console.log(group);
+  renderFixtures: function(group,teams) {
+    var fixturesContainer = $(this.dom.groups).filter('#'+group).find('.fixtures');
+    var template = [
+      '<p class="fixture">'+teams['1'].name+'Vs'+teams['2'].name+'</p>',
+      '<p class="fixture">'+teams['3'].name+'Vs'+teams['4'].name+'</p>',
+      '<p class="fixture">'+teams['1'].name+'Vs'+teams['3'].name+'</p>',
+      '<p class="fixture">'+teams['4'].name+'Vs'+teams['2'].name+'</p>',
+      '<p class="fixture">'+teams['2'].name+'Vs'+teams['3'].name+'</p>',
+      '<p class="fixture">'+teams['4'].name+'Vs'+teams['1'].name+'</p>'
+    ].join("\n");
+    fixturesContainer.append(template);
+    //1v2
+    //3v4
+    //1v3
+    //4v2
+    //2v3
+    //4v1
   }
 
 };
